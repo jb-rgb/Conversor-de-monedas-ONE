54886fcb4a2b537d53f377aff69168b8dc8fb925
Jorge Barahona <jb30085@gmail.com>
2024-07-05 13:42:34 -0600

    Se ha hecho la interacción con el usuario. Falta verificar si se puede optimizar más el código.

--- src/com/aluracursos/conversordemonedas/calculos/CalculosConversion.java
+++ src/com/aluracursos/conversordemonedas/calculos/CalculosConversion.java
@@ -5,12 +5,12 @@ import com.aluracursos.conversordemonedas.modelos.Conversor;
 import java.util.Map;
 
 public class CalculosConversion {
-    public double convertir(String cantidad, String monedaDestino, Conversor conversor) {
+    public double convertir(Double cantidad, String monedaDestino, Conversor conversor) {
         Map<String, Double> tasasDeCambio = conversor.conversion_rates();
         if (!tasasDeCambio.containsKey(monedaDestino)) {
             throw new RuntimeException("La moneda destino " + monedaDestino + " no se encuentra en las tasas de cambio.");
         }
         double cambio = tasasDeCambio.get(monedaDestino);
-        return Double.parseDouble(cantidad) * cambio;
+        return cantidad * cambio;
     }
 }
--- src/com/aluracursos/conversordemonedas/principal/Principal.java
+++ src/com/aluracursos/conversordemonedas/principal/Principal.java
@@ -4,12 +4,88 @@ import com.aluracursos.conversordemonedas.calculos.CalculosConversion;
 import com.aluracursos.conversordemonedas.consultaapi.Consulta;
 import com.aluracursos.conversordemonedas.modelos.Conversor;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
         Consulta consulta = new Consulta();
-        Conversor conversor = consulta.obtenerTasasDeCambio("USD");
+        Conversor conversor;
         CalculosConversion calculos = new CalculosConversion();
-        double cantidadConvertidad = calculos.convertir("25", "ARS", conversor);
-        System.out.println("25 USD son " + cantidadConvertidad + " ARS.");
+        Scanner sc = new Scanner(System.in);
+        int opc = 0;
+        double moneda;
+        double cantidadConvertida;
+
+        while (opc !=  7) {
+            try {
+
+                System.out.println("=================================================");
+                System.out.println();
+                System.out.println("Sea bienvenido/a al Conversor de Moneda =)");
+                System.out.println();
+                System.out.println("1.- Dólar =>> Peso mexicano");
+                System.out.println("2.- Peso mexicano =>> Dólar");
+                System.out.println("3.- Dólar =>> Real brasileño");
+                System.out.println("4.- Real brasileño =>> Dólar");
+                System.out.println("5.- Dólar =>> Peso colombiano");
+                System.out.println("6.- Peso colombiano =>> Dólar");
+                System.out.println("7.- Salir");
+                System.out.println();
+                System.out.print("Ingrese una opción válida: ");
+                opc = sc.nextInt();
+                if (opc == 7) {
+                    break;
+                }
+                System.out.println();
+                System.out.println("=================================================");
+                System.out.println();
+                System.out.print("Ingrese el valor que desea convertir: ");
+                moneda = sc.nextDouble();
+                System.out.println();
+                System.out.println("=================================================\n");
+
+                switch (opc) {
+                    case 1:
+                        conversor = consulta.obtenerTasasDeCambio("USD");
+                        cantidadConvertida = calculos.convertir(moneda, "MXN", conversor);
+                        System.out.println("El valor " + moneda + " [USD] corresponde al valor final de " + cantidadConvertida + " [MXN]\n");
+                        break;
+                    case 2:
+                        conversor = consulta.obtenerTasasDeCambio("MXN");
+                        cantidadConvertida = calculos.convertir(moneda, "USD", conversor);
+                        System.out.println("El valor " + moneda + " [MXN] corresponde al valor final de " + cantidadConvertida + " [USD]\n");
+                        break;
+                    case 3:
+                        conversor = consulta.obtenerTasasDeCambio("USD");
+                        cantidadConvertida = calculos.convertir(moneda, "BRL", conversor);
+                        System.out.println("El valor " + moneda + " [USD] corresponde al valor final de " + cantidadConvertida + " [BRL]\n");
+                        break;
+                    case 4:
+                        conversor = consulta.obtenerTasasDeCambio("BRL");
+                        cantidadConvertida = calculos.convertir(moneda, "USD", conversor);
+                        System.out.println("El valor " + moneda + " [BRL] corresponde al valor final de " + cantidadConvertida + " [USD]\n");
+                        break;
+                    case 5:
+                        conversor = consulta.obtenerTasasDeCambio("USD");
+                        cantidadConvertida = calculos.convertir(moneda, "COP", conversor);
+                        System.out.println("El valor " + moneda + " [USD] corresponde al valor final de " + cantidadConvertida + " [COP]\n");
+                        break;
+                    case 6:
+                        conversor = consulta.obtenerTasasDeCambio("COP");
+                        cantidadConvertida = calculos.convertir(moneda, "USD", conversor);
+                        System.out.println("El valor " + moneda + " [COP] corresponde al valor final de " + cantidadConvertida + " [USD]\n");
+                        break;
+                    case 7:
+                        break;
+                    default:
+                        System.out.println("Opción inválida\n");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("\n=================================================");
+                System.out.println("\nError: Entrada inválida. Por favor, ingrese un número válido.\n");
+                sc.next();
+            }
+        }
     }
 }
